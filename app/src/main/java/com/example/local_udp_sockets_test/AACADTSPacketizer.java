586ac9a19b12d8adc8e278adb82b05514677c4fb
Edward Lu <edwardzlu98@gmail.com>
2019-08-11 16:33:52 -0400

    Implemented proper parsing of AAC ADTS frames from MediaRecorder stream

--- app/src/main/java/com/example/local_udp_sockets_test/AACADTSPacketizer.java
+++ app/src/main/java/com/example/local_udp_sockets_test/AACADTSPacketizer.java
@@ -1,197 +1,132 @@
 package com.example.local_udp_sockets_test;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
-import android.os.SystemClock;
 import android.util.Log;
 
-/**
- *
- *   RFC 3640.
- *
- *   This packetizer must be fed with an InputStream containing ADTS AAC.
- *   AAC will basically be rewrapped in an RTP stream and sent over the network.
- *   This packetizer only implements the aac-hbr mode (High Bit-rate AAC) and
- *   each packet only carry a single and complete AAC access unit.
- *
- */
-public class AACADTSPacketizer extends AbstractPacketizer implements Runnable {
+public class AACADTSPacketizer implements Runnable {
 
     private final static String TAG = "AACADTSPacketizer";
 
-    private final static int MAXPACKETSIZE = 500;
+    private final static int MAX_READ_SIZE = 2000;
 
-    /** There are 13 supported frequencies by ADTS. **/
-    public static final int[] AUDIO_SAMPLING_RATES = {
-            96000, // 0
-            88200, // 1
-            64000, // 2
-            48000, // 3
-            44100, // 4
-            32000, // 5
-            24000, // 6
-            22050, // 7
-            16000, // 8
-            12000, // 9
-            11025, // 10
-            8000,  // 11
-            7350,  // 12
-            -1,   // 13
-            -1,   // 14
-            -1,   // 15
-    };
+    private InputStream is_ = null;
+    private Thread t_;
+    private File cacheDir_;
+    ADTSFrameReadingState readingState_;
 
-    private Thread t;
-    private int samplingRate = 8000;
+    // reference for ADTS header format: https://wiki.multimedia.cx/index.php/ADTS
+    private static class ADTSFrameReadingState {
+        byte[] buffer = new byte[MAX_READ_SIZE];
+        short current_frame_length = 0; // includes ADTS header length
+        int current_bytes_read = 0;
+    }
 
     public AACADTSPacketizer(File cacheDir) {
-        super(cacheDir);
+        cacheDir_ = cacheDir;
+        readingState_ = new ADTSFrameReadingState();
+    }
+
+    public void setInputStream(InputStream is) {
+        this.is_ = is;
     }
 
     public void start() {
-        if (t==null) {
-            t = new Thread(this);
-            t.start();
+        if (t_ ==null) {
+            t_ = new Thread(this);
+            t_.start();
         }
     }
 
     public void stop() {
-        if (t != null) {
+        if (t_ != null) {
             try {
-                is.close();
+                is_.close();
             } catch (IOException ignore) {}
-            t.interrupt();
+            t_.interrupt();
             try {
-                t.join();
+                t_.join();
             } catch (InterruptedException e) {}
-            t = null;
+            t_ = null;
         }
-    }
-
-    public void setSamplingRate(int samplingRate) {
-        this.samplingRate = samplingRate;
     }
 
     public void run() {
 
         Log.d(TAG,"AAC ADTS packetizer started !");
 
-        // "A packet SHALL carry either one or more complete Access Units, or a
-        // single fragment of an Access Unit.  Fragments of the same Access Unit
-        // have the same time stamp but different RTP sequence numbers.  The
-        // marker bit in the RTP header is 1 on the last fragment of an Access
-        // Unit, and 0 on all other fragments." RFC 3640
-
-        // ADTS header fields that we need to parse
-        boolean protection;
-        int frameLength, sum, length, nbau, nbpk, samplingRateIndex, profile;
-        long oldtime = SystemClock.elapsedRealtime(), now = oldtime;
-        byte[] header = new byte[8];
+        byte[] final_adts_frame_buffer;
 
         try {
             while (!Thread.interrupted()) {
 
-                // Synchronisation: ADTS packet starts with 12bits set to 1
-                while (true) {
-                    if ( (is.read()&0xFF) == 0xFF ) {
-                        header[1] = (byte) is.read();
-                        if ( (header[1]&0xF0) == 0xF0) break;
-                    }
+                int read_size = is_.available();
+                if (read_size > MAX_READ_SIZE) {
+                    read_size = MAX_READ_SIZE;
+                }
+                if (read_size <= 0) { continue; }
+
+                Log.d(TAG, "Attempting to read " + read_size + " bytes from input stream.");
+                try {
+                    int ret = is_.read(readingState_.buffer,
+                                       readingState_.current_bytes_read,
+                                       read_size);
+                    if (ret == -1) { break; }
+
+                }
+                catch (IOException e) { e.printStackTrace(); }
+
+                Log.d(TAG, "Current contents of reading state buffer: " +
+                        Helpers.bytesToHex(readingState_.buffer));
+
+                readingState_.current_bytes_read += read_size;
+                Log.d(TAG, "Current bytes read: " + readingState_.current_bytes_read);
+
+                // we've finished reading enough of the header to get the frame length
+                if (readingState_.current_bytes_read >= 5) {
+                    readingState_.current_frame_length =
+                            (short) ((((readingState_.buffer[3]&0x02) << 3 | (readingState_.buffer[4]&0xE0) >> 5) << 8) +
+                                    ((readingState_.buffer[4]&0x1F) << 3 | (readingState_.buffer[5]&0xE0) >> 5));
+                    Log.d(TAG, "Length of current ADTS frame: " +
+                                    readingState_.current_frame_length);
                 }
 
-                // Parse adts header (ADTS packets start with a 7 or 9 byte long header)
-                fill(header, 2, 5);
+                // we've read the entirety of the current ADTS frame, deliver the full adts frame for
+                // processing and set the reading state properly
+                if (readingState_.current_bytes_read >= readingState_.current_frame_length) {
 
-                // The protection bit indicates whether or not the header contains the two extra bytes
-                protection = (header[1]&0x01)>0 ? true : false;
-                frameLength = (header[3]&0x03) << 11 |
-                        (header[4]&0xFF) << 3 |
-                        (header[5]&0xFF) >> 5 ;
-                frameLength -= (protection ? 7 : 9);
+                    final_adts_frame_buffer = Arrays.copyOf(readingState_.buffer, readingState_.current_frame_length);
+                    Log.d(TAG, "ADTS frame read from MediaRecorder stream: " +
+                        Helpers.bytesToHex(final_adts_frame_buffer));
 
-                // Number of AAC frames in the ADTS frame
-                nbau = (header[6]&0x03) + 1;
-
-                // The number of RTP packets that will be sent for this ADTS frame
-                nbpk = frameLength/MAXPACKETSIZE + 1;
-
-                // Read CRS if any
-                if (!protection) is.read(header,0,2);
-
-                samplingRate = AUDIO_SAMPLING_RATES[(header[2]&0x3C) >> 2];
-                profile = ( (header[2]&0xC0) >> 6 ) + 1 ;
-
-                // We update the RTP timestamp
-                ts +=  1024L*1000000000L/samplingRate; //stats.average();
-
-                //Log.d(TAG,"frameLength: "+frameLength+" protection: "+protection+" p: "+profile+" sr: "+samplingRate);
-
-                sum = 0;
-                while (sum < frameLength) {
-
-                    buffer = new byte[500];
-
-                    // Read frame
-                    if (frameLength-sum > MAXPACKETSIZE-4) {
-                        length = MAXPACKETSIZE-4;
+                    // we did not read past the end of the current ADTS frame
+                    if (readingState_.current_bytes_read == readingState_.current_frame_length) {
+                        readingState_.current_bytes_read = 0;
+                        readingState_.current_frame_length = 0;
                     }
-                    else {
-                        length = frameLength-sum;
+                    else { // we did read past the end of the current ADTS frame
+                        int read_over_length = readingState_.current_bytes_read - readingState_.current_frame_length;
+                        System.arraycopy(readingState_.buffer, readingState_.current_frame_length,
+                                         readingState_.buffer, 0,
+                                         read_over_length);
+
+                        readingState_.current_bytes_read = read_over_length;
+                        readingState_.current_frame_length = 0;
                     }
-                    sum += length;
-                    fill(buffer, 4, length);
-
-                    // AU-headers-length field: contains the size in bits of a AU-header
-                    // 13+3 = 16 bits -> 13bits for AU-size and 3bits for AU-Index / AU-Index-delta
-                    // 13 bits will be enough because ADTS uses 13 bits for frame length
-                    buffer[0] = 0;
-                    buffer[1] = 0x10;
-
-                    // AU-size
-                    buffer[2] = (byte) (frameLength>>5);
-                    buffer[3] = (byte) (frameLength<<3);
-
-                    // AU-Index
-                    buffer[3] &= 0xF8;
-                    buffer[3] |= 0x00;
-
-                    File tempAudioFile = File.createTempFile("temp", ".wav", cacheDir);
-                    tempAudioFile.deleteOnExit();
-                    FileOutputStream fos = new FileOutputStream(tempAudioFile);
-                    fos.write(buffer);
-                    fos.close();
 
                 }
 
             }
         } catch (IOException e) {
-            // Ignore
+            e.printStackTrace();
         } catch (ArrayIndexOutOfBoundsException e) {
-            Log.e(TAG,"ArrayIndexOutOfBoundsException: "+(e.getMessage()!=null?e.getMessage():"unknown error"));
             e.printStackTrace();
         }
 
         Log.d(TAG,"AAC ADTS packetizer stopped !");
-
-    }
-
-    private int fill(byte[] buffer, int offset,int length) throws IOException {
-
-        int sum = 0, len;
-        while (sum<length) {
-            len = is.read(buffer, offset+sum, length-sum);
-            if (len<0) {
-                throw new IOException("End of stream");
-            }
-            else sum+=len;
-        }
-
-        Log.d(TAG, "Contents of filled buffer from stream: " + Arrays.toString(buffer));
-
-        return sum;
 
     }
 
--- app/src/main/java/com/example/local_udp_sockets_test/MainActivity.java
+++ app/src/main/java/com/example/local_udp_sockets_test/MainActivity.java
@@ -8,9 +8,15 @@ import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -31,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         ParcelFileDescriptor mParcelRead;
         ParcelFileDescriptor mParcelWrite;
 
-        AMRNBPacketizer mPacketizer = new AMRNBPacketizer(getExternalCacheDir());
+        AACADTSPacketizer mPacketizer = new AACADTSPacketizer(getExternalCacheDir());
 
         // create an array of parcel file descriptors
         try {
@@ -42,14 +48,15 @@ public class MainActivity extends AppCompatActivity {
         mParcelRead = new ParcelFileDescriptor(mParcelFileDescriptors[0]);
         mParcelWrite = new ParcelFileDescriptor(mParcelFileDescriptors[1]);
 
-        // create the recorder object to record audio and write it to the local send socket
+        // create the recorder object to record audio and stream it to the local socket
         MediaRecorder recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-        recorder.setOutputFile(mParcelWrite.getFileDescriptor());
-        //recorder.setOutputFile(getExternalCacheDir().getAbsolutePath() + "/test.wav");
+        recorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setAudioChannels(1);
         recorder.setAudioSamplingRate(8000);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setAudioEncodingBitRate(32000);
+        recorder.setOutputFile(mParcelWrite.getFileDescriptor());
 
         Log.d(TAG, "Recording started...");
         try {
@@ -61,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         InputStream is = new ParcelFileDescriptor.AutoCloseInputStream(mParcelRead);
 
+        // set the packetizer to read the MediaRecorder's stream and packetize its ADTS frames
         mPacketizer.setInputStream(is);
         mPacketizer.start();
 
@@ -75,86 +83,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
-//    Thread streamThread = new Thread(new Runnable(){
-//        @Override
-//        public void run() {
-//            try {
-//
-//                // create the local send socket for sending data
-//                DatagramSocket local_send_socket = new DatagramSocket(
-//                        new InetSocketAddress("127.0.0.1", 50004)
-//                );
-//                Log.d(TAG, "Local send socket created");
-//
-//                // create the local receive socket for receiving data
-//                DatagramSocket local_recv_socket = new DatagramSocket(
-//                        new InetSocketAddress("127.0.0.1", 50005)
-//                );
-//                Log.d(TAG, "Local receive socket created");
-//
-//                // create the array of data to be sent from the local send socket to the local
-//                // receive socket
-//                byte[] sendData = new byte[1024];
-//                for (int i = 0; i < 1024; i++) {
-//                    sendData[i] = (byte) (i % 128);
-//                }
-//
-//                // create the datagram packet to be sent from the local send socket to the
-//                // local receive socket
-//                InetAddress IPAddress = InetAddress.getByName("localhost");
-//                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 50005);
-//
-//                // get the file descriptor of the local send socket and check its validity
-//                ParcelFileDescriptor pfd = ParcelFileDescriptor.fromDatagramSocket(local_send_socket);
-//                FileDescriptor local_send_socket_fd = pfd.getFileDescriptor();
-//                String fd_validity = (local_send_socket_fd.valid()) ? "valid" : "invalid";
-//                Log.d(TAG, "The local send socket's file descriptor was " + fd_validity);
-//
-//                // create the recorder object to record audio and write it to the local send socket
-//                MediaRecorder recorder = new MediaRecorder();
-//                recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//                recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//                //recorder.setOutputFile(getExternalCacheDir().getAbsolutePath() + "/test.wav");
-//                recorder.setOutputFile(local_send_socket_fd);
-//                recorder.setAudioSamplingRate(22050);
-//                recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//
-//                // prepare the recorder object for recording
-//                try {
-//                    recorder.prepare();
-//                } catch (IOException e) {
-//                    Log.d(TAG, "recorder.prepare() failed: " + e.getMessage());
-//                }
-//
-//                // record audio for five seconds
-//                Log.d(TAG, "Started recording...");
-//                recorder.start();
-//                Thread.sleep(5000);
-//                recorder.stop();
-//                Log.d(TAG, "Stopped recording.");
-//
-//                // send the datagram packet from local send socket to local receive socket
-//                //local_send_socket.send(sendPacket);
-//
-//                // create the datagram packet to receive the data from the local send socket
-//                byte[] receiveData = new byte[1024];
-//                DatagramPacket receivePacket = new DatagramPacket(receiveData, 1024);
-//
-//                // set the local receive socket to receive data
-//                local_recv_socket.receive(receivePacket);
-//
-//                Log.d(TAG, "Data received by local receive socket: " + Arrays.toString(receiveData));
-//
-//            } catch (UnknownHostException e) {
-//                Log.e(TAG, "UnknownHostException: " + e.getMessage());
-//            } catch (IOException e) {
-//                Log.e(TAG, "IOException: " + e.getMessage());
-//                e.printStackTrace();
-//            } catch (InterruptedException e) {
-//                Log.e(TAG, "InterruptedException: " + e.getMessage());
-//                e.printStackTrace();
-//            }
-//        }
-//    });
-//    streamThread.start();
